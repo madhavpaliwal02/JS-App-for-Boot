c1f7e616a6b2255730dfd0509460810708592be4
Madhav Paliwal <madhavpaliwal02@gmail.com>
2022-12-20 00:25:28 +0530

    Learning React

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+// import Header from './components/my';
+import { Button } from 'reactstrap';
+import { ToastContainer, toast } from "react-toastify";
+import Home from './components/home';
 
 function App() {
+
+  const btnHandle = () => {
+    toast("this is my first message");
+    toast.success("done" , {position:'top-center' });
+    toast.error("done" , {position:'top-left' });
+  };
   return (
-    <div className="App">
-      <h1>This is our react page</h1>
+    <div>
+      {/* <ToastContainer /> */}
+      <Home />
     </div>
   );
 }
---
+++ demo/src/components/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Jumbotron } from "reactstrap";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Jumbotron>
+                <p>This is home page of react, and we are using Jumbotron for learning.</p>
+            </Jumbotron>
+        </div>
+    )
+};
+
+export default Home;
